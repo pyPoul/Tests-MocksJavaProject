4602a5331f2b91fa95bec3caf3bce110f4edecce
pyPoul <dev.poulpy@gmail.com>
2025-03-16 10:09:00 +0100

    initial commit

---
+++ tests/main/java/HashDictionaryTest.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class HashDictionaryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
